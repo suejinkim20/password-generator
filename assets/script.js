b94ca05c6b2bf338fcef81952fa033112842917e
Suejin Kim <kim.suejin.20@gmail.com>
2021-02-20 09:58:43 -0500

    working on script

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
+// Replaces "your secure password" with generated password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +12,22 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// The function generatePassword() should run with a series of prompts for password criteria
+
+function generatePassword() {
+  var ready = confirm("Would you like to generate a password?")
+  if (ready = true) {
+    alert("Let's set up some parameters:")
+    confirm("Would you like your password to have ")
+  }
+  else {
+    
+  }
+
+
+
+}
