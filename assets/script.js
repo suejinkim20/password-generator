8914755d0e97af220e67a84ff66ab831101e2d4b
Suejin Kim <kim.suejin.20@gmail.com>
2021-02-25 09:41:55 -0500

    changed variables from global to local within the function

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 //Character Arrays
-var passwordContent = [] //empty array to which selected arrays of characters can be pushed
 const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const lowerLetterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperLetterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const charArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 //Variables
-var finalPassword = "";
-var userLength = ""; //user inputs the length of the password
-var passLength = ""; //password length parsed as integer
+// var userLength = ""; //user inputs the length of the password
+// var passLength = ""; //password length parsed as integer
 
 // Write password to the #password input
 // Replaces "your secure password" with generated password
@@ -24,15 +22,17 @@ function writePassword() {
 
 // The function generatePassword() should run with a series of prompts for password criteria
 function generatePassword () {
+  var finalPassword = "";
+  var passwordContent = [];
   // Prompt for number of characters and store in variable "passLength"
   var userLength = prompt("How many characters do you want your password to be? Please choose between 8 and 128 characters.");
-    passLength = parseInt(userLength);
+    var passLength = parseInt(userLength);
     if (passLength < 8 || passLength > 128) {
       alert("Please choose a number between 8 and 128.");
       return
-    } else {
+    } /* else {
       console.log("The password is " + passLength + " characters long.");
-    }
+    }*/
 
   // Prompt for lowercase letters; if user selects true, then for each item in the lowercase array, assign it an index?
   var wantLowercase = confirm("Do you want lowercase letters in your password?");
@@ -71,11 +71,12 @@ function generatePassword () {
     }
 
 
+
 // Using userNumber and responses from prompts above, generate randomized password
 for (var i = 0; i < passLength; i++) { // for each character within the length that the user designated
-  passwordContent[Math.floor(Math.random() * passwordContent.length)];
+  var randomChar = passwordContent[Math.floor(Math.random() * passwordContent.length)];
   console.log("The password content array is: " + passwordContent)
-  finalPassword = finalPassword + passwordContent[Math.floor(Math.random() * passwordContent.length)];
+  finalPassword = finalPassword + randomChar;
 }
   return finalPassword;
 }
